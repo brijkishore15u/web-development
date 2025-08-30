c77f5658bebf27f1e4d86d02e4bd9af5975ad7a1
brijkishore15u <153313301+brijkishore15u@users.noreply.github.com>
2025-08-30 11:44:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+// var a;
+// a=109;
+
+// var a =109;
+// console.log(a);
+// console.log("hello my name is brijkishor banjara");
+// fullname = "Brijkishor Banjara";
+// console.log(fullname);
+// let name="brijkishore";
+// console.log(name);
+// let str = "   good      morning";
+// console.log("lenght=" + str.length);
+// console.log("last index="+ (str.length - 1));
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// let newstr =str.trim();
+// console.log(newstr);
+// let a ;
+// console.log(a+"5");
+
+// console.log(1+"2"+4);
+
+// console.log(typeof null);
+
+// let y=1;
+// console.log(y++); 
+// console.log(y);
+
+// let x=10;
+// console.log(++x 
+
+// let str="good morning";
+// console.log(str)
+// let newstring =  str.substring (2,8);
+// console.log(newstring);
+
+// console.log(str.slice(2,8));
+// console.log(str.slice(2));
+
+
+
+// array = [1,2,3,4,5,6,7,8,9];
+// console.log(array);
+
+// let arr=[10,12.5,undefined,110,"hii"]
+// console.log(arr.length );
+// console.log(arr)
+// for (let i = 0; i <=arr.length-1,i++)
+//     console.log(arr[i],i);
+
+
+
+// console.log(a);
+// console.log(typeof(a));
+
+
+
+
+// let arr=new Array(3);
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = Number(prompt("nter the data"));
+// }
+
+// for (let i = 0; i < arr.length; i++) {
+
+
+//  let arr=[10,12.5,undefined,110,"hii"]
+
+// console.log(arr);
+// let res = arr.join(",");
+// console.log(res,typeof(res));
+
+ 
+
+//concat 2 arrays 
+
+// copy array
+
+
+//normal function 
+//  function greet() {//function declaration
+//      console.log("Good morning");
+//  }
+// greet();
+ 
+
+
+//function expression ,Anonymous function
+// let greet = function(){
+//     console.log("Good morning");
+// }
+// greet();
+
+
+
+//  function sumOfTwoNumbers (a,b){
+//     console.log(a+b);
+// }
+// sumOfTwoNumbers(10,20);
+// sumOfTwoNumbers(1,9)
+
+
+//sdefault parametrized function
+function sumOfTwoNumbers (a,b,c = 0,d = 0){
+    console.log(a+b);//30
+    console.log(c,d) // c =  undefined, d = undefined
+    console.log(a+b+c+d); //NaN (30)
+}
+sumOfTwoNumbers(10,20,90);  
+
+
+
